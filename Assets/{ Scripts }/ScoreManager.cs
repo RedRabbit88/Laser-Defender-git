7fb796ead81ac9faf1e38986ff82eb738773bdbf
RedRabbit88 <6815330+RedRabbit88@users.noreply.github.com>
2017-08-23 02:55:12 +0100

    Stuff (working on how best to implement the GameManager)

---
+++ Assets/{ Scripts }/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    private int scoreCount;
+
+
+	void Start () {
+        scoreCount = 0;
+        ScoreUpdate();
+	}
+
+    public void ScoreAdd(int value)
+    {
+        scoreCount += value;
+        ScoreUpdate();
+    }
+
+    private void ScoreUpdate()
+    {
+        scoreText.text = "Score: " + scoreCount.ToString();
+    }
+
+    private void ScoreReset()
+    {
+        scoreCount = 0;
+        ScoreUpdate();
+    }
+}
